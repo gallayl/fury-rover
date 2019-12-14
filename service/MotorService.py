8270d9a2981353ee02da35886ffc7b35b0d7a701
Gallay Lajos <gallay.lajos@gmail.com>
2019-12-14 13:22:17 +0100

    feat(service):MotorService progress

--- service/MotorService.py
+++ service/MotorService.py
@@ -2,20 +2,45 @@ import sys
 import string
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit()
+kit = MotorKit(address=0x6f)
 
 while True:
     line = sys.stdin.readline()
-    if (line.startswith("motor")):
+    if (line.startswith("set ")):
         [command, motorString, valueString] = line.split(' ')
         [motorId, value] = [int(motorString), float(valueString)]
-        kit[]
+        getattr(kit, "motor" + str(motorId)).throttle = value
         sys.stdout.write("Motor ")
         sys.stdout.write(motorString)
         sys.stdout.write(" has been set to ")
         sys.stdout.write(valueString)
+        sys.stdout.write("\r\n")
         sys.stdout.flush()
+        continue
+    if (line.startswith("setAll")):
+        [command, valueString] = line.split(' ')
+        value = float(valueString)
+        kit.motor1.throttle = value
+        kit.motor2.throttle = value
+        kit.motor3.throttle = value
+        kit.motor4.throttle = value
+        sys.stdout.write("All motors has been set to ")
+        sys.stdout.write(valueString)
+        sys.stdout.write("\r\n")
+        sys.stdout.flush()
+        continue
+    if (line.startswith("stopAll")):
+        kit.motor1.throttle = 0
+        kit.motor2.throttle = 0
+        kit.motor3.throttle = 0
+        kit.motor4.throttle = 0
+        sys.stdout.write("All stopped.")
+        sys.stdout.write("\r\n")
+        sys.stdout.flush()
+        continue
     else:
-        sys.stdout.write("Echo ")
+        sys.stdout.write("Unkown command: ")
         sys.stdout.write(line)
+        sys.stdout.write("\r\n")
         sys.stdout.flush()
+        continue
