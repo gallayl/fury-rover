222a8abba102be0be6598875b1550e4e3cf1fb3d
Gallay Lajos <gallay.lajos@gmail.com>
2020-03-12 18:16:14 +0100

    feat(python services):moved servos to HAT, replaced client libs

--- service/MotorService.py
+++ service/MotorService.py
@@ -1,63 +1,101 @@
 import sys
 import string
-from adafruit_motorkit import MotorKit
-# ToDo:
-from adafruit_servokit import ServoKit
-servoKit = ServoKit(channels=16)
+from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
+import atexit
+
+mh = Raspi_MotorHAT(addr=0x6f, freq=50)
+
+servoMin = 120  # Min pulse length out of 4096
+servoMax = 670  # Max pulse length out of 4096
 
 
-kit = MotorKit(address=0x6f)
+def arduino_map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+
+def turnOffMotors():
+    mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
+
+
+def setMotor(channel, value):
+    motor = mh.getMotor(channel)
+    if (value > 0):
+        motor.run(mh.FORWARD)
+    else:
+        if (value < 0):
+            motor.run(mh.BACKWARD)
+        else:
+            motor.run(mh.RELEASE)
+    value = max(min(abs(value), 255), 0)
+    motor.setSpeed(value)
+    sys.stdout.write("Motor ")
+    sys.stdout.write(str(channel))
+    sys.stdout.write(" has been set to ")
+    sys.stdout.write(str(value))
+    sys.stdout.write("\r\n")
+    sys.stdout.flush()
+
+
+def setServo(channel, value):
+    if (channel not in [0, 1, 14, 15]):
+        sys.stdout.write(str(channel))
+        sys.stdout.write(" is invalid. Has to be 0, 1 ,14 or 15\r\n")
+        sys.stdout.flush()
+        return
+    value = arduino_map(value, 0, 180, servoMin, servoMax)
+    mh.setPwm(channel, 0, value)
+    sys.stdout.write("Servo ")
+    sys.stdout.write(str(channel))
+    sys.stdout.write(" has been set to ")
+    sys.stdout.write(str(value))
+    sys.stdout.write("\r\n")
+    sys.stdout.flush()
+
+
+atexit.register(turnOffMotors)
 
 while True:
     try:
         line = sys.stdin.readline()
-        #format: servo 1=15;2=25;
+        # format: servo 1=15;2=25;
         if (line.startswith("servo ")):
             [command, servos] = line.split(' ')
             servoValues = servos.split(';')
             for servo in servoValues:
                 [servoIdStr, servoValueStr] = servo.split('=')
-                servoKit.servo[int(servoIdStr)].angle = int(servoValueStr)
+                servoId = int(servoIdStr)
+                servoValue = int(servoValueStr)
+                setServo(servoId, servoValue)
             continue
         if (line.startswith("set ")):
             [command, motorString, valueString] = line.split(' ')
-            [motorId, value] = [int(motorString), float(valueString)]
-            getattr(kit, "motor" + str(motorId)).throttle = value
-            sys.stdout.write("Motor ")
-            sys.stdout.write(motorString)
-            sys.stdout.write(" has been set to ")
-            sys.stdout.write(valueString)
-            sys.stdout.write("\r\n")
-            sys.stdout.flush()
+            [motorId, value] = [int(motorString), int(valueString)]
+            setMotor(int(motorString), value)
             continue
         if (line.startswith("setAll ")):
             [command, valueString] = line.split(' ')
-            value = float(valueString)
-            kit.motor1.throttle = value
-            kit.motor2.throttle = value
-            kit.motor3.throttle = value
-            kit.motor4.throttle = value
-            sys.stdout.write("All motors has been set to ")
-            sys.stdout.write(valueString)
-            sys.stdout.write("\r\n")
-            sys.stdout.flush()
+            value = int(valueString)
+            setMotor(1, value)
+            setMotor(2, value)
+            setMotor(3, value)
+            setMotor(4, value)
             continue
         if (line.startswith("stopAll")):
-            kit.motor1.throttle = 0
-            kit.motor2.throttle = 0
-            kit.motor3.throttle = 0
-            kit.motor4.throttle = 0
+            turnOffMotors()
             sys.stdout.write("All stopped.")
             sys.stdout.write("\r\n")
             sys.stdout.flush()
             continue
         if (line.startswith("set4 ")):
             [command, v1s, v2s, v3s, v4s] = line.split(' ')
-            [v1, v2, v3, v4] = [float(v1s), float(v2s), float(v3s), float(v4s)]
-            kit.motor1.throttle = v1
-            kit.motor2.throttle = v2
-            kit.motor3.throttle = v3
-            kit.motor4.throttle = v4
+            [v1, v2, v3, v4] = [int(v1s), int(v2s), int(v3s), int(v4s)]
+            setMotor(1, v1)
+            setMotor(2, v2)
+            setMotor(3, v3)
+            setMotor(4, v4)
             continue
         else:
             sys.stdout.write("Unkown command: ")
